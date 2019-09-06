aa8763fb09a51269f4ed1538938e1591ef76d287
Allan Gonçalves <allangoncalves@outlook.com>
2019-09-06 18:23:35 -0300

    indicator with absolute position

---
+++ src/components/Indicator.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="base has-text-centered" >
+        <!-- Wind -->
+      <img class="abs-position base" src="@/assets/indicators/wind_med.svg" alt />
+      <!-- Clouds -->
+      <img class="abs-position base" src="@/assets/indicators/clouds.svg" alt />
+      <!-- SUN -->
+      <img class="abs-position base" src="@/assets/indicators/sun.svg" alt />
+      <!-- Tide Height -->
+      <img class="abs-position base" src="@/assets/indicators/tide_med.svg" alt />
+      <!-- Waves Frequency -->
+      <img class="abs-position base" src="@/assets/indicators/wave_med.svg" alt />
+      <!-- BASE -->
+      <img class="abs-position base" src="@/assets/indicators/window.svg" alt />
+      <img src="@/assets/indicators/base.svg" class="base" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.abs-position {
+  position: absolute;
+}
+.base{
+    width: auto;
+    height: 300px;
+    border-radius: 3% 3% 20% 20%;
+}
+</style>
