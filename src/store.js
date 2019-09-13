4c76ed05ac7f8586d7dc3c0dfebd9e81a7b2a269
Allan Gonçalves <allangoncalves@outlook.com>
2019-09-13 00:42:38 -0300

    vuex store for the forecast

--- src/store.js
+++ src/store.js
@@ -1,10 +1,52 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
+var moment = require("moment");
 
 Vue.use(Vuex);
 
+const weather_api = axios.create({
+  baseURL: "https://api.worldweatheronline.com/premium/v1/",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  }
+});
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    days: [],
+    currentData: {},
+    currentTime: null
+  },
+  mutations: {
+    setDays(state, days) {
+      state.days = days;
+    },
+    setCurrentData(state, data){
+      state.currentData = data;
+    },
+    setCurrentTime(state){
+      state.currentTime = moment();
+    }
+  },
+  actions: {
+    getForecast({ commit }, payload) {
+      weather_api
+        .get("/marine.ashx", {
+          params: {
+            key: process.env.VUE_APP_SECRET,
+            q: `${payload.lat},${payload.lng}`,
+            tide: "yes",
+            format: "json"
+          }
+        })
+        .then(res => {
+          commit("setDays", res.data.data.weather);
+
+          commit("setCurrentData", )
+
+        });
+    }
+  }
 });
