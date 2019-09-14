d3caffdfd791b54ed05243b5f39f119cfdb90f2e
Allan Gonçalves <allangoncalves@outlook.com>
2019-09-14 16:01:56 -0300

    tabs component added

---
+++ src/components/Tabs.vue
@@ -0,0 +1,51 @@
+<template>
+  <div style="width:90%">
+    <div class="columns is-mobile">
+      <div
+        class="column is-flex is-paddingless tab"
+        style="align-items:center"
+        v-for="(tab, index) in tabs"
+        :key="tab"
+        @click="changeSelection(index)"
+        :style="{backgroundImage: `url('${require(`@/assets/icons/tab_${ selectedTab === index ? 'black': 'blue'}.svg`)}')`}"
+      >
+        <p class="title is-5">{{ tab }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      selectedTab: 0,
+      tabs: ["Altura", "Periodo", "Vento", "Swell"]
+    };
+  },
+  methods: {
+    changeSelection(index) {
+      this.selectedTab = index;
+      this.$emit("selectionChanged", index);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.tab {
+  background-position: top left;
+  background-repeat: no-repeat;
+  background-attachment: center;
+  background-size: contain;
+  height: 70px;
+}
+.home-carousel-item {
+  //   background-image: url("../assets/icons/tab_blue_base.svg");
+  background-position: left;
+  background-repeat: no-repeat;
+  background-attachment: center;
+  background-size: cover;
+}
+</style>
