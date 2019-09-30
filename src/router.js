9814c2f7fb91a6c2df4d490ecb0a57efa50171db
Allan Goncalves <allangoncalves@outlook.com>
2019-09-30 09:50:09 -0300

    dynamic imports

--- src/router.js
+++ src/router.js
@@ -1,10 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Spot from "@/views/Spot";
-import Home from "@/views/Home";
-import News from "@/views/News";
-import FullForecast from "@/views/FullForecast";
-import AccountCreation from "@/views/AccountCreation";
 
 Vue.use(Router);
 
@@ -15,36 +10,35 @@ export default new Router({
     {
       path: "/",
       name: "Home",
-      component: Home,
+      component: () => import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
       meta: { transitionName: "slide", id: 1 }
     },
     {
       path: "/previsao",
       name: "Full Forecast",
-      component: FullForecast,
+      component: () => import(/* webpackChunkName: "forecast" */ "@/views/FullForecast.vue"),
       meta: { transitionName: "slide", id: 2 }
     },
     {
       path: "/picos",
       name: "Spot",
-      component: Spot,
-      meta: { transitionName: "slide", id: 3 }
+      component: () => import(/* webpackChunkName: "spot" */ "@/views/Spot.vue"),
+      meta: { transitionName: "slide", id: 3 },
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // // which is lazy-loaded when the route is visited.
-      // component: () =>
-      //   import(/* webpackChunkName: "about" */ "./views/About.vue")
+      
     },
     {
       path: "/news",
       name: "News",
-      component: News,
+      component: () => import(/* webpackChunkName: "news" */ "@/views/News.vue"),
       meta: { transitionName: "slide", id: 4 }
     },
     {
       path: "/nova",
       name: "Account Creation",
-      component: AccountCreation,
+      component: () => import(/* webpackChunkName: "create" */ "@/views/AccountCreation.vue"),
       meta: { transitionName: "slide", id: 5 }
     }
   ]
