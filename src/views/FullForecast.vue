39269025e66877e8d6c758ce6535f77cefda4e0e
Allan Goncalves <allangoncalves@outlook.com>
2019-11-06 10:41:19 -0300

    forecast with only the tide table

--- src/views/FullForecast.vue
+++ src/views/FullForecast.vue
@@ -1,223 +1,24 @@
 <template>
-  <div class="container">
-    <div class="tile is-ancestor is-vertical">
-      <div class="tile is-parent" style="padding-top:5rem;padding-bottom:3rem">
-        <div class="tile is-child">
-          <search-bar title="PREVISÃO" @spot-selected="spotSelected" />
+  <div class="container" style="padding-top:2rem;padding-bottom:2rem">
+    <div class="columns is-multiline">
+      <div class="column is-full">
+        <search-bar title="PREVISÃO" @spot-selected="spotSelected" />
+      </div>
+      <div class="column is-full">
+        <div class="columns is-gapless is-mobile">
+          <div class="column">
+            <!-- <img src="@/assets/icons/new_wave.svg" alt /> -->
+          </div>
+          <div class="column is-6">
+            <p class="title is-5 has-text-centered">TÁBUA DE MARÉ</p>
+          </div>
+          <div class="column">
+            <!-- <img src="@/assets/icons/new_wave.svg" alt /> -->
+          </div>
         </div>
       </div>
-      <div v-if="currentSpot !== null && days.length > 0">
-        <div class="tile is-parent">
-          <div class="tile is-child is-5">
-            <div class="tile is-parent is-vertical">
-              <div class="tile is-child has-text-centered">
-                <p class="title">{{currentSpot.name}}</p>
-              </div>
-              <div class="tile is-child">
-                <forecast-hud
-                  :waveHeight="waveHeight"
-                  :WindGustKmph="WindGustKmph"
-                  :waterTemp="waterTemp"
-                  :ultraViolet="ultraViolet"
-                  :wavePeriods="wavePeriods"
-                  :swellHeight="swellHeight"
-                  :windDir="windDir"
-                  :tempC="tempC"
-                  :fullDate="fullDate"
-                />
-              </div>
-            </div>
-          </div>
-          <div class="tile is-child">
-            <div class="tile is-parent is-vertical">
-              <div class="tile is-child">
-                <!-- Day selector -->
-                <div class="columns is-mobile">
-                  <div
-                    class="column is-clickable"
-                    v-for="(day, index) in week"
-                    :key="index"
-                    @click="changeDay(index)"
-                  >
-                    <div class="columns is-gapless has-text-centered is-multiline">
-                      <div class="column is-full">
-                        <div
-                          :class="{'has-background-secondary': selectedDay == index, 'has-background-primary': selectedDay != index}"
-                          style="max-height:0.7em;height:0.5em;border-radius:15%"
-                        ></div>
-                      </div>
-                      <div class="column is-full">
-                        <p
-                          class="title is-5 is-size-6-touch"
-                          :class="{'has-text-secondary': selectedDay == index, 'has-text-primary': selectedDay != index }"
-                        >{{index === 0 ? 'HOJE' : day.date}}</p>
-                      </div>
-                      <div class="column is-full">
-                        <p
-                          class="subtitle is-size-7-mobile"
-                          :class="{'has-text-secondary': selectedDay == index, 'has-text-primary': selectedDay != index }"
-                        >{{day.weekDay}}</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="tile is-child">
-                <div class="columns is-multiline">
-                  <div class="column is-9" style="padding-bottom:0px">
-                    <tabs @selectionChanged="selectedChartChange($event)" />
-                  </div>
-                  <div class="column is-full" style="padding-top:0px">
-                    <apexchart width="100%" type="bar" :options="barOptions" :series="selectedData"></apexchart>
-                  </div>
-                </div>
-                <!-- Chart -->
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="tile is-parent">
-          <div class="tile is-child is-5">
-            <!-- Tide -->
-            <div class="tile is-parent is-vertical" style="padding-top:0.32rem">
-              <div class="tile-is-child">
-                <div class="columns is-gapless is-mobile">
-                  <div class="column">
-                    <img src="@/assets/icons/new_wave.svg" alt />
-                  </div>
-                  <div class="column is-6">
-                    <p class="title is-5 has-text-centered">TÁBUA DE MARÉ</p>
-                  </div>
-                  <div class="column">
-                    <img src="@/assets/icons/new_wave.svg" alt />
-                  </div>
-                </div>
-              </div>
-              <div class="tile-is-child">
-                <apexchart
-                  width="100%"
-                  height="100"
-                  type="area"
-                  :options="areaOptions"
-                  :series="tideData"
-                ></apexchart>
-              </div>
-            </div>
-          </div>
-
-          <div class="tile is-child">
-            <div class="columns">
-              <div class="column is-8">
-                <div class="columns is-multiline">
-                  <div class="column is-full">
-                    <div class="columns is-vcentered is-centered is-mobile">
-                      <div class="column is-1 is-flex is-horizontal-end" style="padding-right:0px">
-                        <figure class="image is-24x24">
-                          <img src="@/assets/icons/star.svg" alt />
-                        </figure>
-                      </div>
-                      <div
-                        class="column is-9 has-text-centered"
-                        style="padding-left:0px;padding-right:0px"
-                      >
-                        <p class="title is-5">MELHORES DO DIA</p>
-                      </div>
-                      <div class="column is-1 is-flex is-horizontal-start" style="padding-left:0px">
-                        <figure class="image is-24x24">
-                          <img src="@/assets/icons/star.svg" alt />
-                        </figure>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="column is-full">
-                    <div class="columns is-mobile">
-                      <div
-                        class="column is-flex is-horizontal-center"
-                        v-for="(time, index) in topThree"
-                        :key="index"
-                      >
-                        <div
-                          class="has-text-centered is-flex is-horizontal-center"
-                          :class="time.highest ?  'best-secondary':  'best-primary'"
-                        >
-                          <div>
-                            <p class="title is-6">{{time.time}}</p>
-                            <p class="subtitle is-6">{{`${time.waveHeight}m`}}</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="column">
-                <div class="columns is-mobile has-text-centered-mobile">
-                  <div class="column">
-                    <div class="tile is-parent is-vertical">
-                      <div class="tile is-child">
-                        <div class="columns is-multiline">
-                          <div
-                            class="column is-full is-gapless is-horizontal-center is-flex"
-                            style="padding-top:0px;padding-bottom:0px;"
-                          >
-                            <figure class="image is-32x32">
-                              <img src="@/assets/icons/dawn.svg" alt />
-                            </figure>
-                          </div>
-                          <div
-                            class="column is-full is-gapless has-text-centered"
-                            style="padding-top:0px;"
-                          >
-                            <p>{{sunrise}}</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="tile is-child">
-                        <div class="columns is-multiline">
-                          <div
-                            class="column is-full is-horizontal-center is-flex"
-                            style="padding-bottom:0px;"
-                          >
-                            <figure class="image is-32x32">
-                              <img src="@/assets/icons/twilight.svg" alt />
-                            </figure>
-                          </div>
-                          <div class="column is-full has-text-centered" style="padding-top:0px;">
-                            <p style="padding-bottom:0px">{{sunset}}</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="column">
-                    <div class="tile is-parent is-vertical" style="padding-top:0px">
-                      <div class="tile is-child">
-                        <div class="columns is-multiline is-gapless">
-                          <div class="column is-full is-horizontal-center is-flex">
-                            <figure class="image is-32x32">
-                              <img src="@/assets/icons/humidity.svg" alt />
-                            </figure>
-                          </div>
-                          <div class="column is-full has-text-centered">
-                            <p style="padding-top:0.5rem;">{{humidity}}</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="tile is-child is-horizontal-center is-flex">
-                        <div class="comment has-text-centered is-clickable">
-                          <p style="padding:0.22rem"></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div class="column is-full">
+        <apexchart width="100%" height="420" type="area" :options="areaOptions" :series="tideData"></apexchart>
       </div>
     </div>
   </div>
@@ -315,7 +116,7 @@ export default {
       ];
     },
     tideData() {
-      return [{ name: "Altura da maré (metros)", data: this.tideHourSeries }];
+      return [this.days.length > 0 && this.currentSpot ? { name: "Altura da maré (metros)", data: this.tideHourSeries } : { name: "Altura da maré (metros)", data: [] }];
     },
     topThree() {
       let forecast = this.days[this.selectedDay].hourly.map(element => {
@@ -481,14 +282,13 @@ export default {
             return `${value}m`;
           }
         },
-
         markers: {
           size: 0,
           style: "hollow"
         },
         xaxis: {
           type: "datetime",
-          tickAmount: 14,
+          tickAmount: 12,
           labels: {
             formatter: (value, timestamp) => {
               return moment(timestamp).format("HH[h]");
